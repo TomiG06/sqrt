47302c3357f691544850e2bf4488c232ec9ab147
TomiG06 <thomasgkountas@gmail.com>
2025-08-19 18:52:46 +0300

    initial commit

---
+++ heron.s
@@ -0,0 +1,62 @@
+.intel_syntax noprefix
+
+.section .data
+half: .float 0.5
+abs_mask: .long 0x7FFFFFFF
+
+.section .text
+.global heron
+.global hwsqrt
+
+.type heron, @function
+.type hwsqrt, @function
+
+heron:
+    # prologue
+    push    rbp
+    mov     rbp, rsp
+
+    push rcx
+
+    xor rcx, rcx
+
+    xorps     xmm1, xmm1      # guess -> xmm1
+    movss     xmm2, [half]    # 0.5   -> xmm2
+    xorps     xmm3, xmm3      # tmp   -> xmm3
+
+    # guess = n / 2
+
+    movss  xmm1, xmm0
+    mulss   xmm1, xmm2
+
+.loop_start:
+    inc rcx
+
+    # compute tmp = n / guess
+    movss xmm3, xmm0
+    divss xmm3, xmm1
+
+    # guess_(i+1) = 0.5 * (guess_(i) + tmp)
+    addss xmm1, xmm3
+    mulss xmm1, xmm2
+
+    # computing an error introduces overhead
+    # and Heron's method actually converges fast enough
+    cmp ecx, 12
+    jl .loop_start
+
+    # move final guess to result
+    movss xmm0, xmm1
+
+    # epilogue
+    pop rcx
+    mov     rsp, rbp
+    pop     rbp
+    ret
+
+hwsqrt:
+    # turns out modern CPUs implement sqrt by themselves
+    # no frame needed
+    sqrtss xmm0, xmm0
+    ret
+
---
+++ sqrt.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+#include <math.h>
+
+#define ERROR 1e-6
+#define MAXITERATIONS 30
+#define BENCHITER (int)1e7
+
+
+// basically the same as stdc
+extern float hwsqrt(float n);
+extern float heron(float n);
+
+
+static inline float tomabs(float f) {
+    return f >= 0? f: -f;
+}
+
+uint64_t gettime() {
+    struct timespec t = {};
+    clock_gettime(CLOCK_MONOTONIC, &t);
+    return t.tv_sec * 1e9 + t.tv_nsec;
+}
+
+double delta(uint64_t s, uint64_t e) {
+    return (double)(e-s) * 1e-9;
+}
+
+
+/*
+ *  |g*2 - t| <= E
+ *  
+ *  sqrt(t - E) <= g <= sqrt(t+E)
+ * */
+// binary search implementation
+float binsqrt(float n) {
+    float high = n >= 1? n: 1;
+    float low = 0;
+    float mid = (low+high)/2;
+    size_t iter = 0;
+    do {
+        float msqr = mid*mid;
+        if(msqr < n) {
+            low = mid;
+        } else if(msqr > n) {
+            high = mid;
+        }
+        mid = (high + low)/2;
+        iter++;
+    } while(tomabs(mid*mid - n) > ERROR && iter < MAXITERATIONS); // until acceptable error or MAXITERATIONS reached
+
+    return mid;
+}
+
+void printBench(char* name, float in, float out, double delta) {
+    printf("%15s sqrt(%f) = %f %f s\n", name, in, out, delta);
+}
+
+int main(int argc, char** argv) {
+    if(argc < 2) {
+        printf("Number not provided\n");
+        return 1;
+    }
+
+    float n = strtof(argv[1], NULL);
+    if(n < 0) {
+        printf("Number should be a positive real\n");
+        return 1;
+    }
+    
+    uint64_t s, e;
+    volatile float sr;
+
+    printf("LOOPS: %d\n", BENCHITER);
+
+    s = gettime();
+    for(int i = 0; i < BENCHITER; i++) {
+        sr = sqrt(n);
+    }
+    e = gettime();
+    printBench("stdc", n, sr, delta(s, e));
+    
+    s = gettime();
+    for(int i = 0; i < BENCHITER; i++) {
+        sr = binsqrt(n);
+    }
+    e = gettime();
+    printBench("Binary Search", n, sr, delta(s, e));
+
+    s = gettime();
+    for(int i = 0; i < BENCHITER; i++) {
+        sr = heron(n);
+    }
+    e = gettime();
+    printBench("Heron's Method", n, sr, delta(s, e));
+
+    s = gettime();
+    for(int i = 0; i < BENCHITER; i++) {
+        sr = hwsqrt(n);
+    }
+    e = gettime();
+    printBench("SQRTSS", n, sr, delta(s, e));
+
+    return 0;
+}
+
